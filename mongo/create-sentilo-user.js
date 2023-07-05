25e09c26cad4acb5d77fdf637b7290ce9bb9ed8e
Eduardo <eduardoprasniewski@gmail.com>
2023-07-05 17:39:52 -0300

    Sentilo-core docker

--- mongo/create-sentilo-user.js
+++
@@ -1,6 +0,0 @@
-use sentilo
-db.createUser({
-    user: "sentilo",
-    pwd: "sentilo",
-    roles: [{role: "readWrite", db:"sentilo"}]
-}) 
--- mongo/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mongo < create-sentilo-user.js
-mongo -u sentilo -p sentilo sentilo /sentilo/scripts/mongodb/init_data.js
-mongo -u sentilo -p sentilo sentilo /sentilo/scripts/mongodb/init_test_data.js
