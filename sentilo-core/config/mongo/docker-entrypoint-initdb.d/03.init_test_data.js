aacb1fff479d9221b669a4f2d00053b60b2b281d
Eduardo Prasniewski <eduardoprasniewski@gmail.com>
2023-10-30 10:00:24 -0300

    add extra data to mongodb initialization

--- sentilo-core/config/mongo/docker-entrypoint-initdb.d/03.init_test_data.js
+++ sentilo-core/config/mongo/docker-entrypoint-initdb.d/03.init_test_data.js
@@ -31,6 +31,10 @@ db.componentType.insert({ "_id" : "generic", "_class" : "org.sentilo.web.catalog
 db.componentType.insert({ "_id" : "electricity_meter", "_class" : "org.sentilo.web.catalog.domain.ComponentType", "name" : "Electricity meter", "icon" : "pins2", "createdAt" : ISODate("2013-11-08T10:28:01.226Z") });
 db.componentType.insert({ "_id" : "noise", "_class" : "org.sentilo.web.catalog.domain.ComponentType", "name" : "Noise meter", "icon" : "pins3", "createdAt" : ISODate("2013-11-08T10:28:01.226Z") });
 db.componentType.insert({ "_id" : "meteo", "_class" : "org.sentilo.web.catalog.domain.ComponentType", "name" : "Meteo", "icon" : "pins4", "createdAt" : ISODate("2013-11-08T10:28:01.226Z") });
+db.componentType.insert({ "_id" : "air_quality", "name" : "Air Quality", "description" : "Air Quality control", "icon" : "air", "createdAt" : ISODate("2023-09-27T12:41:24.125Z"), "_class" : "org.sentilo.web.catalog.domain.ComponentType" });
+db.componentType.insert({ "_id" : "temperature", "name" : "Temperature", "description" : "Temperatura", "icon" : "temperatura", "createdAt" : ISODate("2023-09-27T12:44:59Z"), "_class" : "org.sentilo.web.catalog.domain.ComponentType" });
+db.componentType.insert({ "_id" : "humidity", "name" : "Humidity", "description" : "Umidade", "icon" : "sky_spot", "createdAt" : ISODate("2023-09-27T12:50:34Z"), "_class" : "org.sentilo.web.catalog.domain.ComponentType" });
+db.componentType.insert({ "_id" : "wind", "name" : "Anemometer", "description" : "Wind speed", "icon" : "ambient", "createdAt" : ISODate("2023-09-27T17:07:11.035Z"), "_class" : "org.sentilo.web.catalog.domain.ComponentType" });
 
 // Insert sensor types
 print("Load sensor types");
@@ -41,3 +45,12 @@ db.sensorType.insert({ "_id" :"humidity", "_class" : "org.sentilo.web.catalog.do
 db.sensorType.insert({ "_id" :"pluviometer", "_class" : "org.sentilo.web.catalog.domain.SensorType", "name" : "Pluviometer",  "createdAt" : ISODate("2013-11-08T10:27:36.152Z") });
 db.sensorType.insert({ "_id" :"rain", "_class" : "org.sentilo.web.catalog.domain.SensorType", "name" : "Rain", "createdAt" : ISODate("2013-11-08T10:27:36.152Z") });
 db.sensorType.insert({ "_id" :"wind", "_class" : "org.sentilo.web.catalog.domain.SensorType", "name" : "Wind",  "createdAt" : ISODate("2013-11-08T10:27:36.152Z") });
+db.sensorType.insert({ "_id" : "air_quality_pm10", "name" : "PM10", "description" : "", "createdAt" : ISODate("2023-08-30T17:20:34.264Z"), "_class" : "org.sentilo.web.catalog.domain.SensorType" });
+db.sensorType.insert({ "_id" : "air_quality_pm25", "name" : "PM25", "description" : "", "createdAt" : ISODate("2023-08-30T17:20:39.892Z"), "_class" : "org.sentilo.web.catalog.domain.SensorType" });
+db.sensorType.insert({ "_id" : "air_quality_no2", "name" : "NO2", "description" : "Nitrogen dioxide measurement", "createdAt" : ISODate("2023-09-27T17:57:38.565Z"), "_class" : "org.sentilo.web.catalog.domain.SensorType" });
+db.sensorType.insert({ "_id" : "air_quality_pm1", "name" : "PM1", "description" : "Measurement of suspension particles PM1", "createdAt" : ISODate("2023-09-27T17:58:08.401Z"),"_class" : "org.sentilo.web.catalog.domain.SensorType" });
+db.sensorType.insert({ "_id" : "air_quality_o3", "name" : "O3", "description" : "Ozone measurement", "createdAt" : ISODate("2023-09-27T17:58:24.284Z"),"_class" : "org.sentilo.web.catalog.domain.SensorType" });
+db.sensorType.insert({ "_id" : "air_quality_so2", "name" : "SO2", "description" : "Sulfur dioxide measurement", "createdAt" : ISODate("2023-09-27T17:58:39.072Z"), "_class" : "org.sentilo.web.catalog.domain.SensorType" });
+db.sensorType.insert({ "_id" : "air_quality_co", "name" : "CO", "description" : "Carbon Monoxide measurement", "createdAt" : ISODate("2023-09-27T17:58:53.729Z"), "_class" : "org.sentilo.web.catalog.domain.SensorType" });
+db.sensorType.insert({ "_id" : "air_quality_co2", "name" : "CO2", "description" : "Carbon dioxide measurement", "createdAt" : ISODate("2023-09-27T17:59:08.050Z"), "_class" : "org.sentilo.web.catalog.domain.SensorType" });
+db.sensorType.insert({ "_id" : "air_quality_ox", "name" : "OX", "description" : "OX = O3 + NO2", "createdAt" : ISODate("2023-09-27T21:04:40.555Z"), "_class" : "org.sentilo.web.catalog.domain.SensorType" });
